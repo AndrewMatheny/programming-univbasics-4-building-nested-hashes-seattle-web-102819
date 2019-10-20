8e6d707d847de3c4a5b607565729e3c954763792
Andrew Matheny <androidmatheny@gmail.com>
2019-10-20 23:58:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,8 @@ end
 
 def monopoly_with_third_tier
   base_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {
-    one_piece_owned => 25
+    one_piece_owned => 25;
+    two_piec
   }}
 	}
 end
